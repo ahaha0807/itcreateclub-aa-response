2f5fb8064e32b3f2737f25e60ae9807f51a7bf53
ahaha0807 <b5331@oic.jp>
2017-11-04 13:17:16 +0900

    外部に画像URLを投げつけて、AAに変換されてレスポンス返ってくるおもちゃ機能の追加

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let app = express()
 let fs = require('fs')
+let rp = require('request-promise')
 
 app.get('/', (req, res) => {
     fs.readFile('./aa.txt', 'utf8', (err, text) => {
@@ -9,9 +10,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/html', (req, res) => {
-    fs.readFile('./aa.txt', 'utf8', (err, text) => {
-        res.send('<pre>'+ text + '</pre>')
-    })        
+    let isSpecified = req.query.url !== undefined
+
+    if (isSpecified) {
+        rp('http://process.filestackapi.com/' +
+                process.env.FILE_STACK_APIKEY +
+                '/ascii=background:black,size:90,colored:true,reverse:true/' +
+                req.query.url)
+            .then((ascii) => {
+                res.send(ascii)
+            }).catch((err) => {
+                if (process.env.FILE_STACK_APIKEY === undefined) {
+                    res.send('API keyがだめだよ')
+                }
+                res.send('ごめんなさい。何らかの原因でAA化に失敗しました。')
+            })
+    } else {
+        fs.readFile('./itc-logo.html', 'utf8', (err, text) => {
+            res.send(text)
+        })
+    }
 })
 
 app.use(express.static('public'))
